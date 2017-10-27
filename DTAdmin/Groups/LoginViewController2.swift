08ba12d40e0cf96fb53cac13e66d17b040a7e369
Ivan Kravchuk <kravchykivan@gmail.com>
2017-10-27 17:14:27 +0300

    merge

--- DTAdmin/Groups/LoginViewController2.swift
+++ DTAdmin/Groups/LoginViewController2.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController1: UIViewController {
     
     @IBAction func loginButtoTapped(_ sender: Any) {
         HTTPService.login(){(result:HTTPURLResponse) in
