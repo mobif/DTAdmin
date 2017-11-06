23415d1a0217d732f1c66fdeadb6320e42425494
Anastasiia Kinelska <anastasia.kinelska@gmail.com>
2017-11-06 19:24:44 +0200

    [Fix] list representation

--- DTAdmin/Test/Controllers/TestsViewController.swift
+++ DTAdmin/Test/Controllers/TestsViewController.swift
@@ -11,44 +11,60 @@ import UIKit
 class TestsViewController: UIViewController {
     
     var cookie: HTTPCookie?
-    var testsList = [TestStructure]()
+    var testsList: [TestStructure]?
 
+//    var filteredList: [TestStructure]? {
+//        return self.testsList
+//    }
+    
     @IBOutlet weak var testsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        update()
+        self.testsTableView.reloadData()
+    }
+    
+    func update() {
         DataManager.shared.getList(byEntity: .Test) { (tests, error) in
-            print(tests, error)
-//            self.testsList = tests as? [TestStructure]
-//            self.testsTableView.reloadData()
+            
+            guard let tests = tests as? [TestStructure] else {
+                print(error)
+                return
+            }
+            self.testsList = tests as? [TestStructure]
+            self.testsTableView.reloadData()
         }
     }
+
 }
+
 
 extension TestsViewController: UITableViewDelegate {
 }
   
 extension TestsViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int { return 1 }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testsList != nil ? testsList.count : 0  // ternary operator
+        return testsList != nil ? testsList!.count : 0  // ternary operator
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath) as? TestCell else {
             fatalError("The dequeued cell is not an instance of TestCell.")
         }
-        let newTestsList = testsList[indexPath.row]
-        cell.testName.text = newTestsList.name
-        cell.timeForTest.text = newTestsList.timeForTest
-        cell.attempts.text = newTestsList.attempts
+//        guard let test = testsList![indexPath.row] else {
+//            print('Unwrap fail')
+//            return
+//        }
+        cell.testName?.text = testsList![indexPath.row].name
+        cell.timeForTest?.text = testsList![indexPath.row].timeForTest
+        cell.attempts?.text = testsList![indexPath.row].attempts
         return cell
     }
     
-    
-//    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-//        
-//    }
+
 }
     
     
