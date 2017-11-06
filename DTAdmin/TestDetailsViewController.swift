216d56dffb8cd3a298920dc6d8d12fdc81a58411
ITA student <student@itas-mac-mini-2.ddns.softservecom.com>
2017-11-06 17:49:05 +0200

    add test detail storyboard

---
+++ DTAdmin/TestDetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TestDetailsViewController.swift
+//  DTAdmin
+//
+//  Created by ITA student on 11/6/17.
+//  Copyright © 2017 if-ios-077. All rights reserved.
+//
+
+import UIKit
+
+class TestDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
