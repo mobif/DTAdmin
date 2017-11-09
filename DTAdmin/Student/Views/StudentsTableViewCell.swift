1d0f4d8958dacf7d4b6fea7e7546be171d3bfcbe
Volodymyr <v.winner@gmail.com>
2017-11-09 11:56:57 +0200

    minor fix, remove spaces

--- DTAdmin/Student/Views/StudentsTableViewCell.swift
+++ DTAdmin/Student/Views/StudentsTableViewCell.swift
@@ -10,19 +10,15 @@ import UIKit
 
 class StudentsTableViewCell: UITableViewCell {
 
-
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var fName: UILabel!
     @IBOutlet weak var sName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
