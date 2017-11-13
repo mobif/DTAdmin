cb96dc6fe65bea1dd832949101ec3c94c36201f1
mac6 <b.hutsul@grt-team.com>
2017-11-13 12:57:11 +0200

    added logout tab, realized logout from app, added logout icon to tab

---
+++ DTAdmin/LogoutViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LogoutViewController.swift
+//  DTAdmin
+//
+//  Created by mac6 on 11/13/17.
+//  Copyright © 2017 if-ios-077. All rights reserved.
+//
+
+import UIKit
+
+class LogoutViewController: UIViewController {
+
+    @IBOutlet weak var centerTextLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func logoutButtonTapped(_ sender: Any) {
+        self.showLoginScreen()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
