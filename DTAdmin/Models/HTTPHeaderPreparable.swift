594353d7bf114dd5ccb067a7f8535a897baceaa5
Volodymyr <v.winner@gmail.com>
2017-11-26 21:07:46 +0200

    replace DataManager to Protocol

---
+++ DTAdmin/Models/HTTPHeaderPreparable.swift
@@ -0,0 +1,44 @@
+//
+//  HTTPHeaderPreparable.swift
+//  DTAdmin
+//
+//  Created by Володимир on 18.11.17.
+//  Copyright © 2017 if-ios-077. All rights reserved.
+//
+
+import Foundation
+
+enum TypeReqest {
+    case insertData
+    case getRecords
+    case updateData
+    case delete
+    case getOneRecord
+    case getCount
+    case getRecordsRange
+    case getStudentsByGroup
+    case getGroupBySpeciality
+    case getGroupByFaculty
+    case getTestDetailsByTest
+    case getTestsBySubject
+    case getTimeTablesForGroup
+    case getTimeTablesForSubject
+    case getQuestionsByLevelRand
+    case getQuestionIdsByLevelRand
+    case getAnswersByQuestion
+    case countRecordsByTest
+    case getRecordsRangeByTest
+    case getRecordsByTestGroupDate
+    case getResultTestIdsByGroup
+}
+protocol HTTPHeaderPreparable {
+    func getURLReqestForEntityManager(entityStructure: Entities,ids: [String]) -> URLRequest?
+    func getURLReqest(entityStructure: Entities, type: TypeReqest) -> URLRequest?
+    func getURLReqest(entityStructure: Entities, type: TypeReqest, id: String) -> URLRequest?
+    func getURLReqest(entityStructure: Entities, type: TypeReqest, limit: String, offset: String) -> URLRequest?
+    func getURLReqest(entityStructure: Entities, type: TypeReqest, id: String, withoutImages: Bool) -> URLRequest?
+    func getURLReqest(entityStructure: Entities, type: TypeReqest, id: String, limit: String,
+                      offset: String) -> URLRequest?
+    func getURLReqest(entityStructure: Entities, type: TypeReqest, id: String, limit: String, offset: String,
+                      withoutImages: Bool) -> URLRequest?
+}
