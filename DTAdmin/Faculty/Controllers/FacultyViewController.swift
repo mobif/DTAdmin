0ff8b93ca08ce0ddb6b5a98d92723e47b583a215
Volodymyr <v.winner@gmail.com>
2017-12-01 12:42:34 +0200

    1

--- DTAdmin/Faculty/Controllers/FacultyViewController.swift
+++ DTAdmin/Faculty/Controllers/FacultyViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FacultyViewController: ParentViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+class FacultyViewController: ParentViewController {
 
     var facultyArray = [FacultyStructure]()
     var filteredFacultyArray = [FacultyStructure]()
@@ -48,33 +48,20 @@ class FacultyViewController: ParentViewController, UITableViewDataSource, UITabl
         }
         self.refresh.endRefreshing()
     }
- 
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard !searchText.isEmpty else {
-            filteredFacultyArray = facultyArray
-            facultyTableView.reloadData()
-            return
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        guard let createUpdateViewController = UIStoryboard(name: "Faculty",
+            bundle: nil).instantiateViewController(withIdentifier: "CreateUpdateViewController")
+                as? CreateUpdateViewController else { return }
+        self.navigationController?.pushViewController(createUpdateViewController, animated: true)
+        createUpdateViewController.resultModification = { newFacultyty in
+            self.filteredFacultyArray.append(newFacultyty)
+            self.facultyTableView.reloadData()
         }
-        filteredFacultyArray = facultyArray.filter({ $0.name.lowercased().contains(searchText.lowercased()) })
-        facultyTableView.reloadData()
     }
+}
 
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = UIView()
-        view.backgroundColor = UIColor.darkGray
-        let id = UILabel()
-        id.text = NSLocalizedString(FacultyDetails.id.rawValue, comment: "header for id in table")
-        id.textColor = UIColor.white
-        id.frame = CGRect(x: 22, y: 2, width: 20, height: 25)
-        view.addSubview(id)
-        let code = UILabel()
-        code.text = NSLocalizedString(FacultyDetails.name.rawValue, comment: "header for faculty name in table")
-        code.textColor = UIColor.white
-        code.frame = CGRect(x: 140, y: 2, width: 100, height: 25)
-        view.addSubview(code)
-        return view
-    }
-
+extension FacultyViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredFacultyArray.count
     }
@@ -90,47 +77,65 @@ class FacultyViewController: ParentViewController, UITableViewDataSource, UITabl
             UITableViewCellAccessoryType.detailDisclosureButton
         return cell
     }
+}
 
+extension FacultyViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = UIColor.darkGray
+        let id = UILabel()
+        id.text = NSLocalizedString(FacultyDetails.id.rawValue, comment: "header for id in table")
+        id.textColor = UIColor.white
+        id.frame = CGRect(x: 22, y: 2, width: 20, height: 25)
+        view.addSubview(id)
+        let code = UILabel()
+        code.text = NSLocalizedString(FacultyDetails.name.rawValue, comment: "header for faculty name in table")
+        code.textColor = UIColor.white
+        code.frame = CGRect(x: 140, y: 2, width: 100, height: 25)
+        view.addSubview(code)
+        return view
+    }
+    
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let edit = UITableViewRowAction(style: .normal, title: NSLocalizedString("Edit", comment: "title for editing"),
-             handler: { action, indexPath in
-             guard let createUpdateViewController = UIStoryboard(name: "Faculty",
-             bundle: nil).instantiateViewController(withIdentifier: "CreateUpdateViewController")
-                as? CreateUpdateViewController else { return }
-                createUpdateViewController.facultyInstance = self.filteredFacultyArray[indexPath.row]
-                 createUpdateViewController.canEdit = true
-                  createUpdateViewController.resultModification = { updateResult in
-                    self.filteredFacultyArray[indexPath.row] = updateResult
-                    self.facultyTableView.reloadData()
-                  }
-                   self.navigationController?.pushViewController(createUpdateViewController, animated: true)
+            handler: { action, indexPath in
+                guard let createUpdateViewController = UIStoryboard(name: "Faculty",
+                    bundle: nil).instantiateViewController(withIdentifier: "CreateUpdateViewController")
+                        as? CreateUpdateViewController else { return }
+                            createUpdateViewController.facultyInstance = self.filteredFacultyArray[indexPath.row]
+                            createUpdateViewController.canEdit = true
+                            createUpdateViewController.resultModification = { updateResult in
+                                self.filteredFacultyArray[indexPath.row] = updateResult
+                                self.facultyTableView.reloadData()
+                            }
+                self.navigationController?.pushViewController(createUpdateViewController, animated: true)
         })
         let delete = UITableViewRowAction(style: .destructive, title: NSLocalizedString("Delete",
-             comment: "title for deleting"),
-                 handler: { action, indexPath in
-                    let alert = UIAlertController(title: NSLocalizedString("WARNING", comment: "title for alert"),
-                        message: NSLocalizedString("Do you want to delete this speciality?",
-                            comment: "title for alert message"),
-                                preferredStyle: .alert)
+            comment: "title for deleting"),
+                handler: { action, indexPath in
+                let alert = UIAlertController(title: NSLocalizedString("WARNING", comment: "title for alert"),
+                    message: NSLocalizedString("Do you want to delete this speciality?",
+                        comment: "title for alert message"),
+                            preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: NSLocalizedString("YES", comment: "title for ok key"),
-                        style: .default, handler: { (action) in
-                    alert.dismiss(animated: true, completion: nil)
-                    guard let id = self.filteredFacultyArray[indexPath.row].id else { return }
-                        if indexPath.row < self.filteredFacultyArray.count {
-                            DataManager.shared.deleteEntity(byId: id, typeEntity: Entities.faculty) { (deleted, error) in
-                                if let error = error {
-                                    self.showWarningMsg(error.info)
-                                        } else {
-                                            self.filteredFacultyArray.remove(at: indexPath.row)
-                                            tableView.deleteRows(at: [indexPath], with: .top)
-                                            self.facultyTableView.reloadData()
-                                        }
+                          style: .default, handler: { (action) in
+                            alert.dismiss(animated: true, completion: nil)
+                            guard let id = self.filteredFacultyArray[indexPath.row].id else { return }
+                            if indexPath.row < self.filteredFacultyArray.count {
+                                DataManager.shared.deleteEntity(byId: id, typeEntity: Entities.faculty) { (deleted, error) in
+                                    if let error = error {
+                                        self.showWarningMsg(error.info)
+                                    } else {
+                                        self.filteredFacultyArray.remove(at: indexPath.row)
+                                        tableView.deleteRows(at: [indexPath], with: .top)
+                                        self.facultyTableView.reloadData()
                                     }
                                 }
+                            }
                     }))
                     alert.addAction(UIAlertAction(title: NSLocalizedString("NO", comment: "title for cancel key"),
-                        style: .default, handler: { (action) in
-                            alert.dismiss(animated: true, completion: nil)
+                                                  style: .default, handler: { (action) in
+                                                    alert.dismiss(animated: true, completion: nil)
                     }))
                     self.present(alert, animated: true, completion: nil)
         })
@@ -146,24 +151,25 @@ class FacultyViewController: ParentViewController, UITableViewDataSource, UITabl
         } else {
             guard let facultyInfoViewController = UIStoryboard(name: "Faculty",
                 bundle: nil).instantiateViewController(withIdentifier: "FacultyInfoViewController")
-                    as? FacultyInfoViewController else  { return }
+                as? FacultyInfoViewController else  { return }
             facultyInfoViewController.facultyInstance = self.filteredFacultyArray[indexPath.row]
             self.navigationController?.pushViewController(facultyInfoViewController, animated: true)
             
         }
     }
-    
-    @IBAction func addButtonTapped(_ sender: Any) {
-        guard let createUpdateViewController = UIStoryboard(name: "Faculty",
-            bundle: nil).instantiateViewController(withIdentifier: "CreateUpdateViewController")
-                as? CreateUpdateViewController else { return }
-        self.navigationController?.pushViewController(createUpdateViewController, animated: true)
-        createUpdateViewController.resultModification = { newFacultyty in
-            self.filteredFacultyArray.append(newFacultyty)
-            self.facultyTableView.reloadData()
-        }
-    }
-
-    
 }
+
+extension FacultyViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            filteredFacultyArray = facultyArray
+            facultyTableView.reloadData()
+            return
+        }
+        filteredFacultyArray = facultyArray.filter({ $0.name.lowercased().contains(searchText.lowercased()) })
+        facultyTableView.reloadData()
+    }
+}
+
+
 
